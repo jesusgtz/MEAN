326d173cd3f301370e099f3493de54d72e4cf0e9
Jesus Gutierrez <gusj93@hotmail.com>
2018-07-24 00:54:42 -0500

    Registro de usuarios en el backend

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var User = require('../models/user');
+var bcrypt = require('bcrypt-nodejs');
 
 function home(req, res) {
 	res.status(200).send({
@@ -14,7 +15,50 @@ function pruebas(req, res) {
 	});
 }
 
+function saveUser(req, res) {
+	var params = req.body;
+	var user = new User();
+
+	if(params.name && params.surname && params.nick && params.email 
+		&& params.password) {
+
+		user.name = params.name;
+		user.surname = params.surname;
+		user.nick = params.nick;
+		user.email = params.email;
+		user.role = 'ROLE_USER';
+		user.image = null;
+
+		bcrypt.hash(params.password, null, null, (err, hash) => {
+			user.password = hash;
+
+			user.save((err, userStored) => {
+				if(err) { 
+					return res.status(500).send({
+						message: 'Error al guardar el usuario'
+					});
+				}
+
+				if(userStored) {
+					res.status(200).send({
+						user: userStored
+					});
+				} else {
+					res.status(404).send({
+						message: 'No se ha registrado el usuario'
+					})
+				}
+			});
+		});
+	} else {
+		res.status(200).send({
+			message: 'Envía los campos obligatorios'
+		});
+	}
+}
+
 module.exports = {
 	home,
-	pruebas
+	pruebas,
+	saveUser
 }
--- api/routes/user.js
+++ api/routes/user.js
@@ -7,5 +7,6 @@ var api = express.Router();
 
 api.get('/home', userController.home);
 api.get('/pruebas', userController.pruebas);
+api.post('/register', userController.saveUser);
 
 module.exports = api;
