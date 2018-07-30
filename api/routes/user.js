e841321bed3af61892f6b56f2ea44709b5c675f9
Jesus Gutierrez <gusj93@hotmail.com>
2018-07-30 01:15:36 -0500

    Implementacion de middleware para auth tokens

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,11 +2,12 @@
 
 var express = require('express');
 var userController = require('../controllers/user');
+var md_auth = require('../middlewares/authenticated');
 
 var api = express.Router();
 
 api.get('/home', userController.home);
-api.get('/pruebas', userController.pruebas);
+api.get('/pruebas', md_auth.ensureAuth, userController.pruebas);
 api.post('/register', userController.saveUser);
 api.post('/login', userController.loginUser);
 
