cfec1c3507281d4cbb939022de2fbdc47943283f
Jesus Gutierrez <gusj93@hotmail.com>
2018-08-13 15:18:08 -0500

    Implementacion de consulta de usuarios con paginacion

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,7 +3,7 @@
 var User = require('../models/user');
 var bcrypt = require('bcrypt-nodejs');
 var jwt = require('../services/jwt');
-
+var mongoosePaginate = require('mongoose-pagination');
 
 function home(req, res) {
 	res.status(200).send({
@@ -131,11 +131,38 @@ function getUser(req, res) {
 	});
 }
 
+function getUsers(req, res) {
+	var identity_user_id = req.user.sub;
+
+	var page = 1;
+	if(req.params.page) {
+		page = req.params.page;
+	}
+
+	var itemsPerPage = 5;
+
+	User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) => {
+		if(err) return res.status(500).send({
+			message: 'Error en la peticion'
+		});
+
+		if(!users) return res.status(404).send({
+			message: 'No hay usuarios disponibles'
+		});
+
+		return res.status(200).send({
+			users,
+			total,
+			pages: Math.ceil(total/itemsPerPage)
+		});
+	});
+}
 
 module.exports = {
 	home,
 	pruebas,
 	saveUser,
 	loginUser,
-	getUser
+	getUser,
+	getUsers
 }
--- api/routes/user.js
+++ api/routes/user.js
@@ -9,6 +9,7 @@ var api = express.Router();
 api.get('/home', userController.home);
 api.get('/pruebas', md_auth.ensureAuth, userController.pruebas);
 api.get('/user/:id', md_auth.ensureAuth, userController.getUser);
+api.get('/users/:page?', md_auth.ensureAuth, userController.getUsers);
 api.post('/register', userController.saveUser);
 api.post('/login', userController.loginUser);
 
